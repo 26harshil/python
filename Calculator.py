048a9af3dce0dcf513524aa794885f5d9d1b775c
Harshil solanki <146664906+26harshil@users.noreply.github.com>
2025-02-19 10:56:14 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+
+
+def add(a, b):
+    print( a + b)
+
+def sub(a, b):
+    print( a - b)
+
+
+
+def mul(a, b):
+    print(a * b)
+    
+
+def div(a, b):
+    if b != 0:
+        print(a / b)
+    else:
+        print("Error: Division by zero")
